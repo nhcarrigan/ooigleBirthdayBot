a95e913ee3861c49831a19b407a6d82e6745594b
Naomi Carrigan <nhcarrigan@gmail.com>
2022-06-11 11:18:06 -0700

    feat: bblist command

---
+++ src/commands/bblist.ts
@@ -0,0 +1,48 @@
+import { SlashCommandBuilder } from "@discordjs/builders";
+import { MessageEmbed } from "discord.js";
+
+import BirthdayModel from "../database/models/Birthday";
+import { Command } from "../interface/Command";
+import { errorHandler } from "../utils/errorHandler";
+import { generateDateQuery } from "../utils/generateDateString";
+
+export const bblist: Command = {
+  data: new SlashCommandBuilder()
+    .setName("bblist")
+    .setDescription("List recent and upcoming birthdays!"),
+  run: async (bot, interaction) => {
+    try {
+      await interaction.deferReply();
+      const today = new Date();
+      const queryPoint = new Date(
+        `${today.getMonth() + 1}-${today.getDate()}-2000`
+      ).getTime();
+
+      const results = await BirthdayModel.find({
+        birthday: {
+          // go two days early and eight days ahead
+          $gte: queryPoint - 172800000,
+          $lte: queryPoint + 691200000,
+        },
+      });
+
+      if (!results.length) {
+        await interaction.editReply({
+          content: "It looks like there are no nearby birthdays!",
+        });
+        return;
+      }
+
+      const embed = new MessageEmbed();
+      embed.setTitle("Recent and upcoming birthdays!");
+      embed.setDescription(
+        results
+          .map((el) => `<@!${el.userId}>: ${generateDateQuery(el.birthday)}`)
+          .join("\n")
+      );
+      await interaction.editReply({ embeds: [embed] });
+    } catch (err) {
+      await errorHandler(bot, err, "bblist command");
+    }
+  },
+};
